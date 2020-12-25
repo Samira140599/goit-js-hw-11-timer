2b15b5b42af0da867ec2639ff696c4d52390d1f4
Samira140599 <67050302+Samira140599@users.noreply.github.com>
2020-12-25 19:45:02 +0300

    1

--- index.js
+++ index.js
@@ -7,36 +7,33 @@ const refs ={
   secs: document.querySelector('span[data-value="secs"]'),
   }
 
-  const targetDate = new Date('Dec 31, 2020');
+const targetDate = new Date('Dec 31, 2020');
   
-  function updateClockFace(time){
+function updateClockFace(time){
+  function pad(value){
+    return String(value).padStart(2,'0')
+  };
 
-    function pad(value){
-      return String(value).padStart(2, '0')
-        };
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
 
-const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+  refs.days.textContent = days;
+  refs.hours.textContent = hours;
+  refs.mins.textContent = mins;
+  refs.secs.textContent = secs;
+}
+      
+const timer ={
+  start(){
+    setInterval(()=>{
+      const currentTime = Date.now();
+      const deltaTime = (targetDate - currentTime);
+      updateClockFace(deltaTime);
+    }, 1000);
+  }}
 
-refs.days.textContent = days;
-refs.hours.textContent = hours;
-refs.mins.textContent = mins;
-refs.secs.textContent = secs;
-      }
-
-
-  const timer ={
-    start(){
-      setInterval(()=>{
-        const currentTime = Date.now();
-        const deltaTime = (targetDate - currentTime);
-        updateClockFace(deltaTime);
-      }, 1000);
-    }
-  }
-
-  timer.start();
-
+timer.start();
+ 
   
